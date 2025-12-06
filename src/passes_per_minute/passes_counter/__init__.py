ad68f207c338c87534c2a96024316dc9f28d5a19
Antek <antek.nikolajdu@gmail.com>
2025-12-06 21:13:49 +0100

    fix: fix init file

--- src/passes_per_minute/passes_counter/__init__.py
+++ src/passes_per_minute/passes_counter/__init__.py
@@ -1,16 +1,6 @@
-from .competition_manager import fetch_competitions, get_competition_seasons
-from .competition_processor import get_match_counter, process_competition
 from .http_client import get_json
-from .match_processor import MatchProcessor
-from .player_position_stats import draw_plots, print_summary
+
 
 __all__ = [
-    "get_competition_seasons",
-    "process_competition",
-    "fetch_competitions",
-    "get_match_counter",
-    "MatchProcessor",
-    "print_summary",
-    "draw_plots",
     "get_json",
 ]
